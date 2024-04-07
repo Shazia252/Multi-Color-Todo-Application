463edb6ff84605eeae1a64e0f84aa2ac9ce29722
Shazia252 <161409243+Shazia252@users.noreply.github.com>
2024-04-07 13:40:51 -0700

    Adding a Todo List Appliaction

---
+++ main.ts
@@ -0,0 +1,30 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todoList = [];
+let conditions = true;
+
+console.log(chalk.green.bold("\n \tWelcome to CodeWith Shazia - Todo-List Application\n"));
+
+while(conditions){
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input",
+            message: chalk.magenta("Enter your New Task :")
+        }
+    ]);
+    todoList.push(addTask.task);
+    console.log(`${addTask.task} Task added in Todo-list successfully`);
+
+    let addamoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more task ?",
+            default: "False"
+        }
+    ]);
+    conditions = addamoreTask.addmore
+}
+console.log("Your updated Todo.list:" ,todoList);
